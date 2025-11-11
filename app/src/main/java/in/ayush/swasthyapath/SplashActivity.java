1578c8851d70a80f3f93bdaa1d268b8e058f585c
AdityaByte <adityapawar4914@gmail.com>
2025-11-11 17:03:26 +0530

    Initial Commit

---
+++ app/src/main/java/in/ayush/swasthyapath/SplashActivity.java
@@ -0,0 +1,14 @@
+package in.ayush.swasthyapath;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
